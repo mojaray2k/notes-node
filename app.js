12d5f32c184a2eb771a0d54cb93dfc60aab01107
Amen Ra <ara@envistacorp.com>
2018-03-12 03:12:13 -0400

    feat: Adding package.json and node_modules
    This is the first step to make the note taking app a real node app by adding the package.json and
    node_modules to it along with the first third party module lodash.

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-var add = notes.add(4,9);
-console.log('Result: ',add);
+// console.log(_.isString(true));
+// console.log(_.isString('Amen'));
+var filteredArray = _.uniq(['Amen', 1, 'Amen', 1, 2, 3, 4]);
+console.log(filteredArray);
+
+//console.log('Result: ', notes.add(9,4));
 
 // var user = os.userInfo();
 
